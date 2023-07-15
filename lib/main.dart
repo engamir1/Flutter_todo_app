9fae04d060dbe9b95f5ffd49fea3c76ce797defc
engamir1 <engmohamed.amir101@gmail.com>
2023-07-15 05:07:42 +0300

    second edit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,13 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp( home: Scaffold(
-      body: Text("my Todo App"),
+      body: Column(
+        children: [
+          Text("my Todo App"),
+          Text("Second App"),
+
+        ],
+      ),
     ),);
   }
 }
